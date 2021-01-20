61f17b6ccd917d99a98edc2239c7843599dc35e7
sboshar <s.t.boshar@gmail.com>
2021-01-19 22:19:49 -0500

    moved functoins from game to biome

--- client/src/components/Biome.js
+++ client/src/components/Biome.js
@@ -4,17 +4,17 @@ import { PointerLockControls, Stars, Sky, OrbitControls } from 'drei';
 import SimplexNoise from 'simplex-noise';
 import Lights from './Lights';
 import { Canvas } from 'react-three-fiber';
-import biomes from '../helpers/biomes';
+import { generateSettings } from "../helpers/utils"
 import Player from './Player';
+
 const Biome = ({
-  wrapGetHeightAt,
-  wrapCreateMap,
   DIVISIONS,
   SIDE_LENGTH,
-  biome,
+  mood,
+  seed,
 }) => {
   //could provide a seed
-  const simplex = new SimplexNoise();
+  const simplex = new SimplexNoise(seed);
   const {
     colors,
     colorThresholds,
@@ -29,22 +29,80 @@ const Biome = ({
     cloudInfo,
     rockInfo,
     skyInfo: { stars, timeOfDay },
-  } = biome;
+  } = generateSettings(mood);
+
   const { directionalColor, directionalIntensity } = directionalLight;
   const { isFog, fogColor, fogNear, fogFar } = fog;
   const { ambientColor, ambientIntensity } = ambientLight;
   const { cloudNumber, cloudColors, cloudRange } = cloudInfo;
   const { treeNumber, treeLeafColor, treeTrunkColor, treeRange } = treeInfo;
   const { rockNumber, rockColors, rockRange } = rockInfo;
+ 
+  const createMap = (x, z) => {
+    let height = 0;
+    freqs.forEach((freq, i) => {
+      height +=
+        simplex.noise2D(x * freq, z * freq) * amps[i] > sqThresh[i]
+          ? Math.pow(simplex.noise2D(x * freq, z * freq) * amps[i], 2)
+          : simplex.noise2D(x * freq, z * freq) * amps[i];
+    });
+    return height > finalScaleAndThresh[0]
+      ? height * finalScaleAndThresh[1] + finalScaleAndThresh[2]
+      : height + finalScaleAndThresh[2];
+  };
 
-  const createMap = wrapCreateMap(
-    freqs,
-    amps,
-    sqThresh,
-    finalScaleAndThresh,
-    simplex
-  );
-  const getHeightAt = wrapGetHeightAt(createMap);
+  const getHeightAt = (x, z) => {
+    const length = SIDE_LENGTH / DIVISIONS;
+    const floorX = Math.floor(x / length) * length;
+    const floorZ = Math.floor(z / length) * length;
+    const ceilX = Math.ceil(x / length) * length;
+    const ceilZ = Math.ceil(z / length) * length;
+    const distFloors = Math.sqrt(
+      Math.pow(floorX - x, 2) + Math.pow(floorZ - z, 2)
+    );
+    const distCeils = Math.sqrt(
+      Math.pow(ceilX - x, 2) + Math.pow(ceilZ - z, 2)
+    );
+
+    const points = [
+      [ceilX, createMap(ceilX, floorZ), floorZ],
+      [floorX, createMap(floorX, ceilZ), ceilZ],
+
+      //distance between these vectos and choose the smaller
+      distFloors > distCeils
+        ? [ceilX, createMap(ceilX, ceilZ), ceilZ]
+        : [floorX, createMap(floorX, floorZ), floorZ],
+    ];
+
+    // Get two vectors on the plane
+    const v1 = [
+      points[0][0] - points[1][0],
+      points[0][1] - points[1][1],
+      points[0][2] - points[1][2],
+    ];
+    const v2 = [
+      points[0][0] - points[2][0],
+      points[0][1] - points[2][1],
+      points[0][2] - points[2][2],
+    ];
+
+    // Get cross product of two vectors
+    const norm = [
+      [v1[1] * v2[2] - v1[2] * v2[1]],
+      [v1[2] * v2[0] - v1[0] * v2[2]],
+      [v1[0] * v2[1] - v1[1] * v2[0]],
+    ];
+
+    // Dot product of normal and vector to the point should be 0, so
+    // norm[0]*(points[0][0]-x) + norm[1]*(points[0][1]-y) + norm[2]*(points[0][2]-z) = 0
+
+    return (
+      (norm[0] * (points[0][0] - x) + norm[2] * (points[0][2] - z)) /
+        norm[1] +
+      points[0][1]
+    );
+  };
+
   return (
     <Canvas
       shadowMap
--- client/src/helpers/utils.js
+++ client/src/helpers/utils.js
@@ -1,3 +1,4 @@
+import colorSchemes from './colorSchemes';
 const randInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -7,4 +8,144 @@ const randRange = (max, min) => Math.random() * (max - min) + min
 
 const randVal = (arr) => arr[randInt(0, arr.length - 1)]
 
-export { randVal, randRange, randInt }
+const generateSettings = (mood) => {
+  let colors, ambLight, dirLight, fog, stars, timeOfDay, cloudColors;
+  const happyColors = [
+    'yellow',
+    'lightyellow',
+    'orange',
+    'white',
+    'papyawhip',
+  ];
+  const sadColors = ['white', 'lightpurple', 'lightblue'];
+  if (mood === 'happy') {
+    const ambientColor = randVal(happyColors);
+    const directionalColor = randVal(happyColors);
+    colors = randVal(colorSchemes.happy);
+    ambLight = { ambientColor, ambientIntensity: 0.2 };
+    dirLight = { directionalColor, directionalIntensity: 0.5 };
+    fog = { isFog: true, fogColor: 0xadbfc7, fogNear: 1, fogFar: 175 };
+    stars = false;
+    timeOfDay = 'day';
+    cloudColors = ['white', 'papayawhip'];
+  } else if (mood === 'sad') {
+    const ambientColor = randVal(sadColors);
+    const directionalColor = randVal(sadColors);
+    colors = randVal(colorSchemes.sad);
+    ambLight = { ambientColor, ambientIntensity: 0.2 };
+    dirLight = { directionalColor, directionalIntensity: 0.5 };
+    fog = { isFog: true, fogColor: 'black', fogNear: 1, fogFar: 175 };
+    stars = true;
+    timeOfDay = 'night';
+    cloudColors = ['lightgrey', 'grey'];
+    //change this
+  } else {
+    colors = randVal(colorSchemes.happy);
+    ambLight = { ambientColor: 'lightpink', ambientIntensity: 0.2 };
+    dirLight = { directionalColor: 'lightblue', directionalIntensity: 0.5 };
+    fog = { isFog: true, fogColor: 0x422e34, fogNear: 1, fogFar: 175 };
+    //0x57363b
+    stars = true;
+    timeOfDay = 'sunset';
+    cloudColors = ['white', 'lightpink'];
+  }
+  const rockColors = ['grey', 'darkgrey'];
+  const treeTrunkColor = [0x654321];
+  const treeLeafColor = [0x006400, 0x90ee90, 0x9fee90];
+
+  const greenMountains = {
+    colors: colors,
+    colorThresholds: [28, 5, 2, -Infinity],
+    freqs: [randRange(1 / 120, 1 / 80), randRange(1 / 40, 1 / 30)],
+    amps: [randRange(4, 5), 2.5],
+    sqThresh: [1, Infinity],
+    finalScaleAndThresh: [4, 1.5, 2],
+    fog: fog,
+    ambientLight: ambLight,
+    directionalLight: dirLight,
+    rockInfo: { rockNumber: 100, rockColors, rockRange: [1, 7] },
+    cloudInfo: { cloudNumber: 30, cloudRange: [30, 50], cloudColors },
+    treeInfo: {
+      treeNumber: 75,
+      treeRange: [5, 15],
+      treeTrunkColor,
+      treeLeafColor,
+    },
+    skyInfo: {
+      stars,
+      timeOfDay,
+    },
+  };
+  const whiteMountains = {
+    colors: colors.slice(0, 4).concat([colors.slice(3, 5)]),
+    colorThresholds: [28, 23, 18, 6, -Infinity],
+    freqs: [randRange(1 / 60, 1 / 40), randRange(1 / 30, 1 / 20)],
+    amps: [randRange(4, 5), 3],
+    sqThresh: [0, 3],
+    finalScaleAndThresh: [2, 1.7, 7],
+    fog: fog,
+    ambientLight: ambLight,
+    directionalLight: dirLight,
+    rockInfo: { rockNumber: 100, rockColors, rockRange: [-Infinity, 6] },
+    cloudInfo: { cloudNumber: 30, cloudRange: [30, 50], cloudColors },
+    treeInfo: {
+      treeNumber: 75,
+      treeRange: [-Infinity, 9],
+      treeTrunkColor,
+      treeLeafColor,
+    },
+    skyInfo: {
+      stars,
+      timeOfDay,
+    },
+  };
+  const mesa = {
+    colors: colors,
+    colorThresholds: [14, 8, 2, -2, -Infinity],
+    freqs: [randRange(1 / 200, 1 / 150), randRange(1 / 110, 1 / 90)],
+    amps: [4, 8],
+    sqThresh: [6, Infinity],
+    finalScaleAndThresh: [5, 3, 3],
+    fog: fog,
+    ambientLight: ambLight,
+    directionalLight: dirLight,
+    rockInfo: { rockNumber: 70, rockColors, rockRange: [-1, 7] },
+    cloudInfo: { cloudNumber: 30, cloudRange: [30, 50], cloudColors },
+    treeInfo: {
+      treeNumber: 75,
+      treeRange: [0, 7],
+      treeTrunkColor,
+      treeLeafColor,
+    },
+    skyInfo: {
+      stars,
+      timeOfDay,
+    },
+  };
+  const beach = {
+    colors: colors,
+    colorThresholds: [5.5, 3, 1, -2, -Infinity],
+    freqs: [randRange(1 / 150, 1 / 100), randRange(1 / 60, 1 / 40)],
+    amps: [3, 4],
+    sqThresh: [1, Infinity],
+    finalScaleAndThresh: [0, 1.5, 0],
+    fog: fog,
+    ambientLight: ambLight,
+    directionalLight: dirLight,
+    rockInfo: { rockNumber: 50, rockColors, rockRange: [3, Infinity] },
+    cloudInfo: { cloudNumber: 30, cloudRange: [20, 30], cloudColors },
+    treeInfo: {
+      treeNumber: 50,
+      treeRange: [4, Infinity],
+      treeTrunkColor,
+      treeLeafColor,
+    },
+    skyInfo: {
+      stars,
+      timeOfDay,
+    },
+  };
+  return randVal([whiteMountains, beach, greenMountains, mesa]);
+};
+
+export { randVal, randRange, randInt, generateSettings }
